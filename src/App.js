20edeeb66d9a7aba95513be08c03dc5d19259fc7
Jimmy <dudj23@gmail.com>
2025-07-11 11:56:46 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PayrollProductionUpdater from './PayrollProductionUpdater';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PayrollProductionUpdater />
+    </div>
+  );
+}
+
+export default App;
