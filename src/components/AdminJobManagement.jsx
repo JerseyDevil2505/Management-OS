b6238ae5c21afc7624f36267e75f4a82486fc38d
Jimmy <dudj23@gmail.com>
2025-07-19 01:01:36 -0400

    Update AdminJobManagement.jsx

--- src/components/AdminJobManagement.jsx
+++ src/components/AdminJobManagement.jsx
@@ -271,7 +271,7 @@ const AdminJobManagement = ({ onJobSelect }) => {
             ...job,
             status: job.status === 'active' ? 'Active' : (job.status || 'Active'),
             county: capitalizeCounty(job.county),
-            percentBilled: job.percent_billed || 0.00
+            percentBilled: job.percent_billed || 
           }));
           
           const processedArchivedJobs = archived.map(job => ({
