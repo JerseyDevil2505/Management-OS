be1f71205df7b70412ea7aec1342a06ea4d34962
Jimmy <dudj23@gmail.com>
2025-08-09 12:51:47 -0400

    Update MarketAnalysis.jsx

--- src/components/job-modules/MarketAnalysis.jsx
+++ src/components/job-modules/MarketAnalysis.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
 import { supabase, interpretCodes } from '../../lib/supabaseClient';
 import { 
   AlertCircle, 
